a1fd5546d729d577640a3acf9a8812ebfb39502f
Mohamed Shaker <m.elzantot@gmail.com>
2024-10-30 12:23:35 +0300

    project task crud operations have been added

---
+++ Queros.ProjectManagement.API/Controller/ProjectTaskController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Queros.ProjectManagement.DTOs;
+using Queros.ProjectManagement.Services;
+
+namespace Queros.ProjectManagement.Controller;
+
+[Route("project-task")]
+[ApiController]
+public class ProjectTaskController : Microsoft.AspNetCore.Mvc.Controller
+{
+    private readonly ILogger _logger;
+    private readonly ProjectTaskService _projectTaskService;
+
+    public ProjectTaskController(ILogger<ProjectTaskController> logger, ProjectTaskService projectTaskService)
+    {
+        _logger = logger;
+        _projectTaskService = projectTaskService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateTask(NewTaskRequest taskRequest)
+    {
+        try
+        {
+            var createTaskResult = await _projectTaskService.CreateNewTaskAsync(taskRequest);
+
+            return createTaskResult.IsSuccess
+                ? Ok(createTaskResult)
+                : BadRequest(new { errors = createTaskResult.Errors.Select(x => x.Message).ToList()});
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProjectTask(Guid id)
+    {
+        try
+        {
+            var taskResult = await _projectTaskService.GetProjectTasksAsync(id);
+
+            return taskResult.Count > 0
+                ? Ok(taskResult)
+                : NoContent();
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpPut("{id}/assign-to/{assigneeId}")]
+    public async Task<IActionResult> AssignProjectTask(Guid taskId, Guid assigneeId)
+    {
+        try
+        {
+            var assignmentResult = await _projectTaskService.AssignProjectTaskAsync(taskId, assigneeId);
+
+            return assignmentResult.IsSuccess
+                ? Accepted()
+                : BadRequest(new { errors = assignmentResult.Errors.Select(x => x.Message).ToList() });
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateProjectTask(Guid id, NewTaskRequest taskRequest)
+    {
+        try
+        {
+            var updateResult = await _projectTaskService.UpdateProjectTaskAsync(id, taskRequest);
+
+            return updateResult.IsSuccess
+                ? Accepted()
+                : BadRequest(new { errors = updateResult.Errors.Select(x => x.Message).ToList()});
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    
+}
---
+++ Queros.ProjectManagement/Services/ProjectTaskService.cs
@@ -0,0 +1,66 @@
+﻿using FluentResults;
+using Queros.ProjectManagement.Data.Enums;
+using Queros.ProjectManagement.Data.Models;
+using Queros.ProjectManagement.Data.Repositories;
+using Queros.ProjectManagement.DTOs;
+
+namespace Queros.ProjectManagement.Services;
+
+public class ProjectTaskService(IProjectTaskRepository projectTaskRepository)
+{
+    public async Task<Result> CreateNewTaskAsync(NewTaskRequest taskRequest)
+    {
+       var result =  await projectTaskRepository.AddAsync(new ProjectTask()
+        {
+            ProjectId = taskRequest.ProjectId,
+            Name = taskRequest.Name,
+            Description = taskRequest.Description,
+            Status = ProjectTaskStatus.NotStarted,
+            Priority = taskRequest.Priority,
+            StartDate = taskRequest.StartDate,
+            EndDate = taskRequest.EndDate,
+            AssigneeId = taskRequest.AssigneeId
+        });
+
+        return Result.Ok();
+    }
+
+    public async Task<List<ProjectTaskDto>> GetProjectTasksAsync(Guid projectId)
+    {
+        return await projectTaskRepository.GetAllAsync(x => new ProjectTaskDto()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Description = x.Description,
+            Status = x.Status,
+            StartDate = x.StartDate,
+            EndDate = x.EndDate,
+            Assignee = new UserDto()
+            {
+                Name = x.Name,
+                Id = x.AssignedTo.Id,
+            }
+        },
+            predicate: x => x.ProjectId == projectId);
+    }
+
+    public async Task<Result> UpdateProjectTaskAsync(Guid id, NewTaskRequest taskRequest)
+    {
+        var result = await projectTaskRepository.UpdateAsync(id, x =>
+        {
+            x.Name = taskRequest.Name;
+            x.Description = taskRequest.Description;
+            x.StartDate = taskRequest.StartDate;
+            x.EndDate = taskRequest.EndDate;
+            x.AssigneeId = taskRequest.AssigneeId;
+        });
+
+        return Result.Ok();
+    }
+
+    public async Task<Result> AssignProjectTaskAsync(Guid taskId, Guid assigneeId)
+    {
+        var result = await projectTaskRepository.UpdateAsync(taskId, x => x.AssigneeId = assigneeId);
+        return Result.Ok();
+    }
+}
