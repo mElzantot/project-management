b4c9c1b694656568f8210b8626db520325ae4843
Mohamed Shaker <m.elzantot@gmail.com>
2024-10-30 12:24:36 +0300

    Project management service is now registered

---
+++ Queros.ProjectManagement/Install.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Queros.ProjectManagement.Processors;
+using Queros.ProjectManagement.Services;
+
+namespace Queros.ProjectManagement;
+
+public static class Install
+{
+    public static IServiceCollection AddProjectManagementServices(this IServiceCollection services)
+    {
+        services.AddScoped<IHashingService, HashingService>();
+        services.AddScoped<ITokenServiceProvider, TokenServiceProvider>();
+        services.AddScoped<ProjectService>();
+        services.AddScoped<ProjectTaskService>();
+        services.AddScoped<AuthenticationService>();
+        return services;
+    }
+}
