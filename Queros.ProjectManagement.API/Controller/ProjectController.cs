3d2f830495d119c444c0a7f834f38a563b721d8a
Mohamed Shaker <m.elzantot@gmail.com>
2024-10-30 12:23:16 +0300

    project crud operation has been added

---
+++ Queros.ProjectManagement.API/Controller/ProjectController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Queros.ProjectManagement.DTOs;
+using Queros.ProjectManagement.Services;
+using Subsbase.Balance.Inputs;
+
+namespace Queros.ProjectManagement.Controller;
+
+[Route("project")]
+[ApiController]
+public class ProjectController : Microsoft.AspNetCore.Mvc.Controller
+{
+    private readonly ILogger _logger;
+    private readonly ProjectService _projectService;
+
+    public ProjectController(ILogger<ProjectController> logger, ProjectService projectService)
+    {
+        _logger = logger;
+        _projectService = projectService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateProject(NewProjectRequest projectRequest)
+    {
+        try
+        {
+            var createProjectResult = await _projectService.CreateNewProject(projectRequest);
+
+            return createProjectResult.IsSuccess
+                ? Ok(createProjectResult.Value)
+                : BadRequest(new { errors = createProjectResult.Errors.Select(x => x.Message).ToList()});
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProject(Guid id)
+    {
+        try
+        {
+            var projectsResult = await _projectService.GetProjectAsync(id);
+
+            return projectsResult != null
+                ? Ok(projectsResult)
+                : BadRequest(new { errors = $"Project with Id: {id} doesn't exist"});
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllProjects([FromQuery] PaginationInput pagination)
+    {
+        try
+        {
+            var projectsResult = await _projectService.GetProjectsAsync(pagination);
+
+            return projectsResult.Count > 0
+                ? Ok(new {data = projectsResult})
+                : NoContent();
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+    
+    
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateProject(Guid id, NewProjectRequest projectRequest)
+    {
+        try
+        {
+            var updateResult = await _projectService.UpdateProjectAsync(id, projectRequest);
+
+            return updateResult.IsSuccess
+                ? Accepted()
+                : BadRequest(new { errors = updateResult.Errors.Select(x => x.Message).ToList()});
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+}
---
+++ Queros.ProjectManagement/Services/ProjectService.cs
@@ -0,0 +1,72 @@
+﻿using FluentResults;
+using Queros.ProjectManagement.Data.Enums;
+using Queros.ProjectManagement.Data.Models;
+using Queros.ProjectManagement.Data.Repositories;
+using Queros.ProjectManagement.DTOs;
+using Subsbase.Balance.Inputs;
+
+namespace Queros.ProjectManagement.Services;
+
+public class ProjectService(IProjectRepository projectRepository)
+{
+    public async Task<Result<Guid>> CreateNewProject(NewProjectRequest projectRequest)
+    {
+       var result =  await projectRepository.AddAsync(new Project
+        {
+            Name = projectRequest.Name,
+            Description = projectRequest.Description,
+            Budget = projectRequest.Budget,
+            Status = ProjectStatus.NotStarted,
+            StartDate = projectRequest.StartDate,
+            EndDate = projectRequest.EndDate
+        });
+
+        return Result.Ok();
+    }
+
+    public async Task<List<ProjectDto>> GetProjectsAsync(PaginationInput pagination)
+    {
+        return await projectRepository.GetAllAsync(x => new ProjectDto()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Budget = x.Budget,
+                Status = x.Status,
+                StartDate = x.StartDate,
+                EndDate = x.EndDate,
+                Owner = x.Owner
+            },
+            skip: (pagination.PageNumber - 1) * pagination.PageSize,
+            limit: pagination.PageSize);
+    }
+
+    public async Task<ProjectDto?> GetProjectAsync(Guid id)
+    {
+        return await projectRepository.GetFirstAsync(x => new ProjectDto()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Budget = x.Budget,
+                Status = x.Status,
+                StartDate = x.StartDate,
+                EndDate = x.EndDate
+            },
+            x => x.Id == id);
+    }
+
+    public async Task<Result> UpdateProjectAsync(Guid id, NewProjectRequest projectRequest)
+    {
+        var result = await projectRepository.UpdateAsync(id, x =>
+        {
+            x.Name = projectRequest.Name;
+            x.Description = projectRequest.Description;
+            x.Budget = projectRequest.Budget;
+            x.StartDate = projectRequest.StartDate;
+            x.EndDate = projectRequest.EndDate;
+        });
+
+        return Result.Ok();
+    }
+}
