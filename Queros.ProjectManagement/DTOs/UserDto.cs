89877b372a35f01c8a27419e7ab214cacb10a9b0
Mohamed Shaker <m.elzantot@gmail.com>
2024-10-30 12:22:54 +0300

    user dto has been added

---
+++ Queros.ProjectManagement/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using Queros.ProjectManagement.Data.Enums;
+
+namespace Queros.ProjectManagement.DTOs;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
+}
